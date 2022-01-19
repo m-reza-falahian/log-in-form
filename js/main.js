e36200f8981775848920546727de7b1f319d1f48
m-reza-f <mohammadrf886@gmail.com>
2022-01-19 22:50:27 +0330

    add help_article and footer

---
+++ js/main.js
@@ -0,0 +1,3 @@
+const help_art = document.getElementById("help-art")
+function show_help_article() { help_art.style.display = "flex"; }
+function hide_help_article() { help_art.style.display = "none"; }
